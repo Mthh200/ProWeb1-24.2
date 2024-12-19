fc03a9ab1269d1c957dcb9b4cca3b6698c8cb6e8
Matheus Pereira Menezes <124296642+Mthh200@users.noreply.github.com>
2024-12-19 17:26:11 -0300

    Delete Express/ExpressRouter directory

--- Express/ExpressRouter/routes/about.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.send('<h1>About</h1>');
-    console.log('Página About acessada');
-})
-
-module.exports = router;
--- Express/ExpressRouter/routes/users/signup.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.send('<h1>Users - Signup</h1>');
-  console.log('Página Users - Signup acessada');
-});
-
-module.exports = router;
